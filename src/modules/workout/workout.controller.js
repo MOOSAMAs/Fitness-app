2c1c4b020c25d448fe3e6304040cb4f6c613fafa
Mohamed Osama <medoosos129@gmail.com>
2024-11-04 05:59:23 +0200

    fix some errors

--- src/modules/workout/workout.controller.js
+++ src/modules/workout/workout.controller.js
@@ -1,5 +1,6 @@
 import { clientModel } from "../../../dataBases/models/client.model.js";
 import { trainerModel } from "../../../dataBases/models/trainer.model.js";
+import { workOutModel } from "../../../dataBases/models/workout.model.js";
 import { handleError } from "../../middleware/handleError.js";
 import { messageError } from "../../utils/customError.js";
 import * as workout from "../handlers/controller.handle.js";
@@ -13,7 +14,7 @@ const addWorkoutPlan = handleError(async(req , res , next)=>{
     if(!trainer){
         return next(new messageError('Trainer not found' , 404))
     }
-    const result = new dietModel(req.body)
+    const result = new workOutModel(req.body)
     await result.save()
     res.status(200).json({message:'Workout plan added' , result})
 })
@@ -24,16 +25,16 @@ const getWorkoutPlan = handleError(async(req , res , next)=>{
     if(!client){
         return next(new messageError('Please enroll with trainer first'))
     } 
-    const program = await dietModel.findById(id)
+    const program = await workOutModel.findById(id)
     !program && next(new messageError('You dont have any workout plans' , 404))
     program && res.status(200).json({message:'Your Workout:' , program})
 })
 
-const getAllWorkoutPlans = workout.getAllDocuments(dietModel)
+const getAllWorkoutPlans = workout.getAllDocuments(workOutModel)
 
-const updateWorkoutPlan = workout.updateDocument(dietModel)
+const updateWorkoutPlan = workout.updateDocument(workOutModel)
 
-const deleteWorkoutPlan = workout.deleteDocument(dietModel)
+const deleteWorkoutPlan = workout.deleteDocument(workOutModel)
 
 
 export {
