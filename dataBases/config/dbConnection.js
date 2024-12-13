05a0045eb899bb1ddecb5e1dfc8f39abb674541c
Mohamed Osama <medoosos129@gmail.com>
2024-12-13 08:34:14 +0200

    Fix Issues

--- dataBases/config/dbConnection.js
+++ dataBases/config/dbConnection.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose"
 
 export const dbConnection = ()=>{
-    mongoose.connect(process.env.DB_CONNECTION).then(()=>{
+    mongoose.connect("mongodb+srv://mohamed178:Mohamed@cluster0.dbycv4m.mongodb.net/Fitness-App").then(()=>{
         console.log('Database Connected Successfully.....');
     }).catch((err)=>{
         console.log('ERROR DB NOT FOUND !');
